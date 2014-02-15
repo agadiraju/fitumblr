000394b066f7408225905be6039208cebd69e488
Abhishek Gadiraju <abhishek.gadiraju@gmail.com>
2014-02-15 18:13:20 -0500

    first app.js, skeleton fitbit api done

---
+++ app.js
@@ -0,0 +1,38 @@
+var env = process.env.NODE_ENV || 'production',
+	config = require('./config')[env];
+
+
+var express = require('express);
+	http = require('http'),
+	OAuth = require('oauth'),
+	passport = require('passport');
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'ejs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.session());
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(app.router);
+
+var IndexController = require('./controllers/index'),
+	FitbitAuthController = require('./controllers/fitbit-auth'),
+	FitbitApiController = require('./controllers/fitbit-api');
+
+app.get('/', IndexController.index);
+app.get('/auth/fitibit', passport.authenticate('fitbit'));
+app.get('/auth/fitbit/callback', passport.authenticate('fitbit', {failureReDirect: '/?error=auth-failed' }),
+	function(req, res) {
+		res.redirect('/');
+	}
+);
+
+
+http.createServer(app).listen(app.get('port'), function() {
+	console.log('express server listening on port ' + app.get('port'));
+});
--- controllers/fitbit-api.js
+++ controllers/fitbit-api.js
@@ -1,3 +1,5 @@
+var fs = require('fs'),
+	OAuth = require('oauth');
 var env = process.env.NODE_ENV || 'production';
 var config = require('../config.js')[env];
 var moment = require('moment');
@@ -11,15 +13,76 @@ var oauth = new OAuth.OAuth('https://api.fitbit.com/oauth/request_token',
 		    'HMAC-SHA1'
 );
 
-function getWeekStats
+function getWeekStats(encodedId, callback, api_call) {
+	console.log("Getting weekly stats for", encodedId);
+	
+        var week = [];
+	var counter = 0;
+	for (var i = 1; i < 8; i++)
+	{
+        	today = (moment().subtract('days', i).format("YYYY-MM-DD"));
+		oauth.get(
+			'https://api.fitbit.com/1/user/-/" + api_call + "/date/' + today + '.json',
+			user.accessToken,
+			user.accessSecret,
+			function(err, data, res) {
+				if (err) {
+					console.error("Error fetching activity data", err);
+					callback(err);
+					return ;
+				}
+				
+				data = JSON.parse(data);
+				console.log("stats for " + today, data);
+				day = [today, data];
+				week.push(day);
+				if (week.length == 7) {
+					callback(err, week, api_call);
+				}
+			}
+		);
+	}
+} 
 
-//ask jeremy: the fuck is this
-function notificationsReceived(req, res)
-{
-    res.send(204);
-    
-    //verify it's Fitibit
-    fs.read
+function dataCallback(err, weekData, api_param) {
+	if (err)
+	{
+		console.error("dataCallback error", err);
+		return ;
+	}
+	
+	console.log(weekData);	
 }
 
+function notificationsReceived(req, res) {
+	// Immediately send HTTP 204 No Content
+	res.send(204);
+
+	// TODO: Verify req.headers['x-fitbit-signature'] to ensure it's Fitbit
+
+	fs.readFile(req.files.updates.path, {encoding: 'utf8'}, function (err, data) {
+		if (err) console.error(err);
+		data = JSON.parse(data);
+
+		// [
+		// 	 {
+		// 		collectionType: 'activities',
+		// 		date: '2013-10-21',
+		// 		ownerId: '23RJ9B',
+		// 		ownerType: 'user',
+		// 		subscriptionId: '23RJ9B-all'
+		// 	}
+		// ]
+
+		for (var i = 0; i < data.length; i++) {
+			console.log(data[i]);
+			getWeekStats(data[i].ownerId, dataCallback, "activities");
+			getWeekStats(data[i].ownerId, dataCallback, "sleep");
+		}
+	});
+};
+
+
+module.exports.notificationsReceived = notificationsReceived;
 module.exports.getWeekStats = getWeekStats;
+
