8b74410f574fa0980c87ba636af255f475c13249
Clara Wu <s.clara.wu@gmail.com>
2014-02-16 05:21:04 -0500

    api calls now in parallel using async. correct info is retrieved successfully

--- controllers/fitbit-api.js
+++ controllers/fitbit-api.js
@@ -3,6 +3,8 @@ var fs = require('fs'),
 var env = process.env.NODE_ENV || 'production';
 var config = require('../config.js')[env];
 var moment = require('moment');
+var async = require('async');
+
 
 var oauth = new OAuth.OAuth('https://api.fitbit.com/oauth/request_token', 
 		    'https://api.fitit.com/oauth/access_token',
@@ -29,9 +31,7 @@ function getActivityStats(token, secret, callback, api_call) {
 			}
 				
 			data = JSON.parse(data);
-			console.log("activity stats for last seven days" + data);
-
-			callback(err, data, api_call);
+			callback(null, data);
 			
 		}
 	);
@@ -41,6 +41,7 @@ function getSleepStats(token, secret, callback, api_call) {
 	console.log("Getting sleep stats");
 	
 	var today = (moment().utc().format("YYYY-MM-DD"));
+    console.log('https://api.fitbit.com/1/user/-/sleep/' + api_call + '/date/' + today + '/7d.json');
 	oauth.get(
 		'https://api.fitbit.com/1/user/-/sleep/' + api_call + '/date/' + today + '/7d.json',
 		token,
@@ -53,40 +54,38 @@ function getSleepStats(token, secret, callback, api_call) {
 			}
 				
 			data = JSON.parse(data);
-			console.log("sleep stats for last seven days" + data);
-
-			callback(err, data, api_call);
+			
+			callback(null, data);
 			
 		}
 	);
 }
 
 
-
-
-function dataCallback(err, weekData, api_param) {
-	if (err)
-	{
-		console.error("dataCallback error", err);
-		return ;
-	}
-	
-	console.log("in callback")
-	console.log(weekData);	
-}
-
 function getFitbitData(req, res) {
 	// Immediately send HTTP 204 No Content
-	res.send(204);
 
   console.log(req.user.token_info[0]);
   console.log(req.user.token_info[1]);
 
   var token = req.user.token_info[0];
   var secret = req.user.token_info[1];
-  getActivityStats(token, secret, dataCallback, "steps");
-  getSleepStats(token, secret, dataCallback, "efficiency");
-	// TODO: Verify req.headers['x-fitbit-signature'] to ensure it's Fitbit
+  async.parallel([
+    function(callback) {
+      activityStats = getActivityStats(token, secret, callback, "steps");
+    },
+    function(callback) {
+      SleepStats = getSleepStats(token, secret, callback, "efficiency");
+    }
+  ], function(err, results) { //This function gets called after the two tasks have called their "task callbacks"
+    if (err) return next(err);
+    console.log("activity stats for last seven days" + results[0]);
+    console.log("sleep stats for last seven days" + results[1]);
+    res.render('../views/graph.ejs', {results: results});
+  });  
+
+
+  	// TODO: Verify req.headers['x-fitbit-signature'] to ensure it's Fitbit
 
 	/*fs.readFile(req.files.updates.path, {encoding: 'utf8'}, function (err, data) {
 		if (err) console.error(err);
